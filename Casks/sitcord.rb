bbe574a925e11ed53311d4dd50ece8369f59f60d
Jonathan Ming <jming422@gmail.com>
2021-11-12 14:07:48 -0800

    Sitcord cask first version

---
+++ Casks/sitcord.rb
@@ -0,0 +1,13 @@
+cask "sitcord" do
+  version :latest
+  sha256 :no_check
+
+  url "https://raw.githubusercontent.com/reveel-it/sitcord/main/Sitcord.zip"
+  name "sitcord"
+  desc "Automate (dis)connecting from a Discord server when locking/sleeping and unlocking/waking"
+  homepage "https://github.com/reveel-it/sitcord"
+
+  depends_on cask: "discord"
+
+  app "Sitcord.app"
+end
